3c17aa862024caf66eb0969d51ad2df450bdd48f
Vik-Allen <88082358+Vik-Allen@users.noreply.github.com>
2023-08-04 11:34:50 +0300

    123

--- Calc_2.py
+++ Calc_2.py
@@ -1,23 +1,32 @@
-what=input('Что делаем? (+,-,*,/,**,//):')
-a=float(input('Введите первое число:'))
-b=float(input('Введите второе число:'))
-if what=='+':
-    c=a+b
+what=input('Что делаем? (+, -, *, /, **, //): ')
+
+a=float(input('Введите первое число: '))
+
+b=float(input('Введите второе число: '))
+
+if what == '+':
+    c = a + b
     print('Результат:'+str(c))
-elif what=='-':
-    c=a-b
+
+elif what == '-':
+    c = a - b
     print('Результат:'+str(c))
-elif what=='*':
-    c=a*b
+
+elif what == '*':
+    c = a * b
     print('Результат:'+str(c))
-elif what=='/':
-    c=a/b
+
+elif what == '/':
+    c = a  /b
     print('Результат:'+str(c))
-elif what=='**':
-    c=a**b
+
+elif what == '**':
+    c = a ** b
     print('Результат:'+str(c))
-elif what=='//':
-    c=a//b
+
+elif what == '//':
+    c = a // b
     print('Результат:'+str(c))
+
 else:
     print('Выбрана неверная операция!')
