31d32647fd598994f2bb34fe1e2030fec6cf8142
Vik-Allen <88082358+Vik-Allen@users.noreply.github.com>
2023-08-15 10:21:18 +0300

    Add files via upload

---
+++ prog-fit.py
@@ -0,0 +1,11 @@
+from appium import webdriver
+import time
+
+desired_caps = {
+    'platformName': 'Android',
+    'platformVersion': '13',
+    'deviceName': 'OnePlus 8 Pro',
+    'appPackage': 'ru.handh.sportlife',
+    'appActivity': ''
+}
+
